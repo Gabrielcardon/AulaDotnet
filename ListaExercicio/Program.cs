5ad86d6e707e8f644e70a4285c760b8f38ed15d9
Gabrielcardon <gcardon13@gmail.com>
2022-08-11 20:13:12 -0300

    exercicios

---
+++ ListaExercicio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ListaExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Imprimir uma mensagem no terminal
+            Console.WriteLine("Digite a altura");
+            //ler uma nova teclado
+            
+            int altura = Console.ReadLine();
+
+            Console.WriteLine("Digite a largura");
+            
+        }
+    }
+}
